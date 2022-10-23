e79e1635a26d9129b41df87eae29979206c5dc20
Vidura Vijerathne <viduranox@gmail.com>
2022-10-23 08:44:04 +0530

    sign in ,sign up process

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function Navigate_User_signin(){
+    window.location = "pages/user/signin.php"
+
+}
+
+function Navigate_User_signup(){
+    window.location = "pages/user/signup.php"
+}
---
+++ pages/user/signup.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Boostrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <!-- linking Boostrap icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+
+
+
+
+    <section class="vh-100">
+        <div class="container py-1 h-100 ">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col col-xl-9">
+
+
+                    <!-- start card -->
+                    <div class="col-12 offset-lg-2 col-lg-8 offset-md-1 col-md-10 card rounded-5  ">
+                        <div class="row g-0 ">
+
+
+                            <!-- start contend right side -->
+                            <div class="col-lg-12 ">
+                                <div class="card-body p-md-1 mx-md-4">
+                                    <div class="card-body">
+
+                                        <h2 class="mt-2 mb-4 text-center "> <u> New Accounting</u></h2>
+
+
+                                        <div class=" row m-2">
+
+                                            
+                                            <div class="col-12 col-lg-12 col-md-12 mb-4">
+                                                <div class="form-outline">
+                                                    <input type="text" id="name" class="form-control" placeholder="User Name" />
+
+                                                </div>
+                                            </div>
+
+
+
+
+                                            <div class="col-12 col-lg-6 col-md-6 mb-4">
+                                                <div class="form-outline">
+                                                    <input type="text" id="name" class="form-control" placeholder="First Name" />
+
+                                                </div>
+                                            </div>
+
+                                            <div class="col-12 col-lg-6 col-md-6 mb-4">
+                                                <div class="form-outline">
+                                                    <input type="text" id="name" class="form-control" placeholder="Last Name" />
+
+                                                </div>
+                                            </div>
+
+
+
+
+                                            <div class="col-12 col-lg-12 col-md-12 mb-4">
+                                                <div class="form-outline">
+                                                    <input type="text" id="schoolName" class="form-control" placeholder="School Name" />
+
+                                                </div>
+                                            </div>
+
+
+                                            <div class="col-12 col-lg-6 col-md-6 mb-4">
+                                                <div class="form-outline">
+                                                    <input type="text" id="wNumber" class="form-control" placeholder="Whatsapp Number" />
+
+                                                </div>
+                                            </div>
+
+
+
+
+                                            <div class="col-12 col-lg-6 col-md-6 mb-4">
+                                                <div class="form-outline">
+                                                    <input type="text" id="tNumber" class="form-control" placeholder="Telegram Number" />
+
+                                                </div>
+                                            </div>
+
+
+
+                                            <div class="col-12  mb-4">
+                                                <input type="email" id="email" class="form-control" placeholder="Email address" />
+
+                                            </div>
+                                            
+                                            <div class="col-12  mb-4">
+                                                <input type="email" id="email" class="form-control" placeholder="Email address Again" />
+
+                                            </div>
+
+                                            <div class="col-12  mb-4">
+                                                <input type="email" id="address" class="form-control" placeholder="Address" />
+
+                                            </div>
+
+
+
+
+                                            <div class="col-12 col-lg-6 col-md-6 mb-4">
+                                                <div class="form-outline">
+                                                    <input type="text" id="password" class="form-control" placeholder="Password" />
+
+                                                </div>
+                                            </div>
+
+                                            <div class="col-12 col-lg-6 col-md-6 mb-4">
+                                                <div class="form-outline">
+                                                    <input type="text" id="password" class="form-control" placeholder="Password Again" />
+
+                                                </div>
+                                            </div>
+
+                                            <div class="col-6 mb-4">
+                                                <div class="form-outline">
+                                                    <select id="gender" class="form-select">
+                                                        <option>Gender</option>
+
+
+
+
+                                                    </select>
+
+
+                                                </div>
+                                            </div>
+
+
+                                            <div class="col-6 mb-4">
+                                                <div class="form-outline">
+                                                    <select id="grade" class="form-select">
+                                                        <option>Grade</option>
+
+
+
+
+
+
+                                                    </select>
+
+
+                                                </div>
+                                            </div>
+
+                                            <div class="col-6 mb-4">
+                                                <div class="form-outline">
+                                                    <select id="province" class="form-select">
+                                                        <option>Province</option>
+
+
+
+
+                                                    </select>
+
+
+                                                </div>
+                                            </div>
+
+                                            <div class="col-6 mb-4">
+                                                <div class="form-outline">
+                                                    <select id="Destrict" class="form-select">
+                                                        <option>Destrict</option>
+
+
+                                                
+
+
+
+
+
+                                                    </select>
+
+
+                                                </div>
+                                            </div>
+
+
+
+
+
+
+
+
+
+                                        </div>
+
+
+
+                                        <div class="pt-1 mb-4 col-12 d-grid text-center ">
+                                            <button class="btn btn-dark btn-lg btn-block rounded-5" onclick="signup();" type="button">
+                                                signup
+                                            </button>
+                                        </div>
+                                        <span> Already Have an Account ? <a href="signin.php" class="text-decoration-none text-danger fw-bolder text-capitalize" > sign in</a></span>
+                                        
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+    </section>
+
+
+
+    <!-- end login -->
+
+
+
+    <!-- Boostrap Java Script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+        </script>
+
+
+
+
+
+
+</body>
+
+</html>
